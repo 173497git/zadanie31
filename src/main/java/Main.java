5e41ca2713c734b05654b87abdf82f2507fdf496
173497git <161622376+173497git@users.noreply.github.com>
2024-04-17 14:39:45 +0000

    zad 3-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,33 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        System.out.println("Podaj znak:");
+        char znak = scanner.next().charAt(0);
+
+        System.out.println("Podaj liczbę wierszy:");
+        int liczbaWierszy = scanner.nextInt();
+
+        System.out.println("Podaj liczbę kolumn:");
+        int liczbaKolumn = scanner.nextInt();
+
+        // Generowanie macierzy
+        char[][] macierz = new char[liczbaWierszy][liczbaKolumn];
+        for (int i = 0; i < liczbaWierszy; i++) {
+            for (int j = 0; j < liczbaKolumn; j++) {
+                macierz[i][j] = znak;
+            }
+        }
+
+        // Wyświetlanie macierzy
+        System.out.println("Wygenerowana macierz:");
+        for (int i = 0; i < liczbaWierszy; i++) {
+            for (int j = 0; j < liczbaKolumn; j++) {
+                System.out.print(macierz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
